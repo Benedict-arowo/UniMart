1ad8a8851329bd4eb6268127e8715021d1851a87
Benedict_arowo <benedict.arowo@gmail.com>
2025-02-19 09:12:07 +0100

    Remove unused Request import in product controller

--- server/controllers/product.controller.ts
+++ server/controllers/product.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import ProductService from "../services/product.service";
 import { Req } from "../utils/types";
